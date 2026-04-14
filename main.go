3ee8c54fd728b896dbaa61101269e831cc7f8707
A4BHI <manthukafc@gmail.com>
2026-04-15 00:22:20 +0530

    hmm

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	app.New()
+	a := app.New()
+	mainWindow := a.NewWindow("fyne-scan")
+
+	label := widget.NewLabel("Fyne-Scanner")
+	label.Alignment = fyne.TextAlignCenter
+	label.TextStyle = fyne.TextStyle{Bold: true, Italic: true}
+
+	mainWindow.SetContent(container.NewCenter(label))
+	mainWindow.Resize(fyne.NewSize(400, 300))
+
+	mainWindow.ShowAndRun()
+
 }
